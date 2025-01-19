e2f96bc900d2c48c012cde56ba0f9b31f5c96b80
eapata8 <113617783+eapata8@users.noreply.github.com>
2025-01-18 21:56:22 -0500

    router settings

--- Web application/frontend/src/App.jsx
+++ Web application/frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <h1>Hello developpers!</h1>
+      <Outlet/>
     </>
   )
 }
