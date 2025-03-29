86f8d286e7b5ddc6e9243d4c851d1a43ee416abc
bAnalya <bayamanal12@gmail.com>
2025-03-28 23:09:30 -0400

    updates

--- Cart code/ServoControl.py
+++ Cart code/ServoControl.py
@@ -1,17 +1,23 @@
-from gpiozero import Servo
+from gpiozero import AngularServo
 import time
 
 class ServoControl:
     def __init__(self, pin):
-        self.servo = Servo(pin, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
+        self.servo = AngularServo(
+            pin,
+            min_angle=0,
+            max_angle=180,
+            min_pulse_width=0.5/1000,
+            max_pulse_width=2.5/1000
+        )
 
     def set_position(self, direction):
         if direction == "left":
-            self.servo.min()
+            self.servo.angle = 30   # ou 0 si tu veux extrême gauche
         elif direction == "center":
-            self.servo.mid()
+            self.servo.angle = 90
         elif direction == "right":
-            self.servo.max()
+            self.servo.angle = 150  # ou 180 pour extrême droite
         else:
             return
         time.sleep(0.4)  # laisser le temps au servo de se stabiliser
