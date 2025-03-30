41bc839b5140bbc2a0d2549b2154a3e8731f8ea0
Jenny-Carl <jennycarlk@gmail.com>
2025-03-30 16:33:51 -0400

    Update main_control.py

--- Cart code/main_control.py
+++ Cart code/main_control.py
@@ -1,5 +1,5 @@
 # main_control.py
-import time
+from time import sleep
 import serial
 import RPi.GPIO as GPIO
 from UltrasonicSensor import UltrasonicSensor
@@ -10,137 +10,121 @@ GPIO.setwarnings(False)
 GPIO.cleanup()
 print("GPIO Clean up")
 
-# Configuration
-#TRIG = 6
-#ECHO = 21
-SERVO_PIN = 17
-#sensor = UltrasonicSensor(TRIG, ECHO)
-servo = ServoControl(SERVO_PIN)
+class main_control:
+    def __init__(self):
+        TRIG = 6
+        ECHO = 4
+        SERVO_PIN = 17
+        sensor = UltrasonicSensor(TRIG, ECHO)
+        servo = ServoControl(SERVO_PIN)
 
-LEFT_SENSOR = 20
-MIDDLE_SENSOR = 16
-RIGHT_SENSOR = 26
-IR01_sensor = LineSensor(LEFT_SENSOR)
-IR02_sensor = LineSensor(MIDDLE_SENSOR)
-IR03_sensor = LineSensor(RIGHT_SENSOR)
+        LEFT_SENSOR = 20
+        MIDDLE_SENSOR = 16
+        RIGHT_SENSOR = 26
+        IR01_sensor = LineSensor(LEFT_SENSOR)
+        IR02_sensor = LineSensor(MIDDLE_SENSOR)
+        IR03_sensor = LineSensor(RIGHT_SENSOR)
 
-# Communication série vers ESP32
-ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
-time.sleep(2)
+        # Communication série vers ESP32
+        ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+        sleep(1)
+        ser.reset_input_buffer()
 
-def measure_at(direction):
-    servo.set_position(direction)
-    return sensor.get_distance()
+        # Front Left Motor
+        F_en_a = 12
+        # Front Right Motor
+        F_en_b = 18
 
-def get_line_position():
-    LMR = 0x00
-    if IR01_sensor.value:
-        LMR |= 4
-    if IR02_sensor.value:
-        LMR |= 2
-    if IR03_sensor.value:
-        LMR |= 1
-    return LMR
-
-def follow_line():
-    LMR = get_line_position()
-    
-    # if LMR == 2:
-    #     ser.write(b"F\n")  # Avancer
-    # elif LMR == 4 or LMR == 6:
-    #     ser.write(b"L\n")  # Tourner gauche
-    # elif LMR == 1 or LMR == 3:
-    #     ser.write(b"R\n")  # Tourner droite
-    # elif LMR == 7:
-    #     ser.write(b"S\n")  # Stop : perdu la ligne
-    # else:
-    #     ser.write(b"S\n") 
-
-def avoid_obstacle(left_distance, right_distance):
-    print("Obstacle détecté → évitement en cours")
-
-    # Reculer légèrement
-    ser.write(b"B\n")
-    time.sleep(0.8)
-
-    if left_distance > right_distance:
-        # Tourner à gauche
-        ser.write(b"L\n")
-        time.sleep(0.8)
-    else:
-        # Tourner à droite
-        ser.write(b"R\n")
-        time.sleep(0.8)
-
-    # Avancer un peu
-    ser.write(b"F\n")
-    time.sleep(1)
-
-    # Tourner pour tenter de retrouver la ligne
-    ser.write(b"L\n" if left_distance > right_distance else b"R\n")
-    time.sleep(1)
-
-    # Avancer doucement jusqu'à retrouver la ligne
-    while True:
-        LMR = get_line_position()
-        if LMR != 0:
-            print("Ligne retrouvée !")
-            break
-        ser.write(b"F\n")
-        time.sleep(0.2)
-
-# tester le Ultrasonic sensor
-def test_ultrasonic_servo():
-    try:
-        while True:
-            for direction in ["left", "center", "right"]:
-                servo.set_position(direction)
-                dist = sensor.get_distance()
-                print(f"{direction.capitalize()} distance: {dist} cm")
-                time.sleep(0.5)
-    except KeyboardInterrupt:
-        print("\nFin du test.")
+        # Back Left Motor
+        B_en_a = 19
+        # Back Right Motor
+        B_en_b = 13
 
 
-if __name__ == '__main__':
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(F_en_a,GPIO.OUT)
+        GPIO.setup(B_en_a,GPIO.OUT)
 
-    # test_ultrasonic_servo() 
-    """ try:
-            test_ultrasonic_servo() 
-        except KeyboardInterrupt:
-            GPIO.cleanup()
-            ser.write(b"S\n")
-            print("Arrêt manuel") """
+        GPIO.setup(F_en_b,GPIO.OUT)
+        GPIO.setup(B_en_b,GPIO.OUT)
 
-    # test de follow_line()
-    print("Mode : SUIVI DE LIGNE UNIQUEMENT (pas d'obstacle)")
-    try:
-        while True:
-            follow_line()
-            time.sleep(0.2)  # plus réactif
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-        ser.write(b"S\n")
-        print("Arrêt manuel")
+        frontL_wheel = GPIO.PWM(F_en_a,1000)
+        frontR_wheel = GPIO.PWM(F_en_b,1000)
+        backL_wheel = GPIO.PWM(B_en_a,1000)
+        backR_wheel = GPIO.PWM(B_en_b,1000)
+        frontL_wheel.start(0)
+        frontR_wheel.start(0)
+        backL_wheel.start(0)
+        backR_wheel.start(0)
 
-    # test final
-    """ try:
-        while True:
-            left = measure_at("left")
-            center = measure_at("center")
-            right = measure_at("right")
+    def set_motor_speed(self, speedFL, speedFR, speedBL, speedBR):
+        self.frontL_wheel.ChangeDutyCycle(speedFL)
+        self.frontR_wheel.ChangeDutyCycle(speedFR)
+        self.backL_wheel.ChangeDutyCycle(speedBL)
+        self.backR_wheel.ChangeDutyCycle(speedBR)
 
-            print(f"Gauche: {left} | Centre: {center} | Droite: {right}")
+    def measure_at(self, direction):
+        self.servo.set_position(direction)
+        return self.sensor.get_distance()
 
-            if center < 20:
-                ser.write(b"S\n")
-                avoid_obstacle(left, right)
+    def get_line_position(self):
+        LMR = 0x00
+        if self.IR01_sensor.value:
+            LMR |= 4
+        if self.IR02_sensor.value:
+            LMR |= 2
+        if self.IR03_sensor.value:
+            LMR |= 1
+        return LMR
+
+    def follow_line(self):
+        LMR = self.get_line_position()
+
+        if LMR == 2:
+            self.ser.write(b"F\n")  # Avancer
+        elif LMR == 4:
+            self.ser.write(b"L\n")  # Tourner gauche
+        elif LMR == 6:
+            self.ser.write(b"L\n")  # Tourner gauche
+        elif self.LMR==1:
+            self.ser.write(b"R\n")  # Tourner droite
+        elif LMR == 3:
+            self.ser.write(b"R\n")  # Tourner droite
+        elif LMR == 7 or LMR == 0:
+            self.ser.write(b"S\n")  # Stop : perdu la ligne
+
+    def avoid_obstacle(self, left_distance, right_distance):
+        print("Obstacle détecté → évitement en cours")
+
+        # Reculer légèrement
+        self.ser.write(b"B\n")
+        sleep(0.8)
+
+        if left_distance >= right_distance:
+            # Tourner à gauche
+            self.ser.write(b"L\n")
+            sleep(0.8)
+            self.ser.write(b"F\n")
+            sleep(0.8)
+            self.ser.write(b"R\n")
+        else:
+            # Tourner à droite
+            self.ser.write(b"R\n")
+            sleep(0.8)
+            self.ser.write(b"F\n")
+            sleep(0.8)
+            self.ser.write(b"L\n")
+
+        sleep(0.3)
+
+        # Avancer doucement jusqu'à retrouver la ligne
+        while not (self.IR01_sensor.value or self.IR02_sensor.value or self.IR03_sensor.value):
+            self.ser.write(b"F\n")
+
+            if left_distance >= right_distance:
+                self.ser.write(b"R\n")
             else:
-                follow_line()
+                self.ser.write(b"L\n")
+        self.follow_line()
+    
 
-            time.sleep(1)
-
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-        ser.write(b"S\n")
-        print("Arrêt manuel") """
